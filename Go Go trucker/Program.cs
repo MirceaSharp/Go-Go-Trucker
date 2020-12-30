aee27037b08095ff83bd1d3fb51021a5f35ffd33
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2020-12-30 23:21:34 +0100

    adding files

---
+++ Go Go trucker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Go_Go_trucker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wat is jouw snelheid?");
+            double snelheid = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hoeveel km heb je gereden?");
+            double afstand = Convert.ToDouble(Console.ReadLine());
+
+            double aantalUurGereden = afstand / snelheid;
+            Console.WriteLine("Je hebt " + aantalUurGereden + " uur gereden");
+            Console.ReadLine();
+
+
+        }
+    }
+}
